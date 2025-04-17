78728b2f71d437a21f76183cab3f47c58b98284c
Vinay Negi <vinegi004@gmail.com>
2025-04-17 17:36:03 +0530

    Add layout for auth pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex-center h-screen">
       <h1 className="text-brand">Hello</h1>
     </div>
   );
